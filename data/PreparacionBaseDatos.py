ed5bec1b0dfcff7e06dab5a63bd89d4e61a2d2dd
gatuno1 <8146845+gatuno1@users.noreply.github.com>
2025-02-06 13:23:55 -0300

    Corr. librerías con aviso obsolescencia y seguridad en claves. - Actualización de código para usar variables de entorno para llaves API - Actualización de librerías de LangChain con aviso de obsolescencia, llevadas a versiones Modernas. - Mejoras de formateo del código, de manejo de path en Windows y uso de impresión con formato, para mejor debug.

--- data/PreparacionBaseDatos.py
+++ data/PreparacionBaseDatos.py
@@ -1,23 +1,38 @@
- # Este modulo se ocupa de la preparación e importación de datos en un archivo .csv hasta cargarlos en una base de datos en Redis a utilizar como contexto para las consultas del usuario 
+#!/usr/bin/env python
+
+"""Este modulo se ocupa de la preparación e importación de datos en un archivo .csv, hasta cargarlos en una base de datos en Redis, que se utiliza como contexto para las consultas del usuario."""
 
 import os
-import pandas as pd 
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores.redis import Redis
-from langchain.document_loaders import TextLoader
-from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+
+import pandas as pd
 from langchain.text_splitter import CharacterTextSplitter
-from dotenv import load_dotenv
-import numpy as np
-import openai
-from redis.commands.search.query import Query
-import redis
-import os
+from langchain_community.document_loaders import TextLoader
+from langchain_community.vectorstores.redis import Redis
+from langchain_openai.embeddings import OpenAIEmbeddings
+from rich import print, traceback  # sobreescritura de la función print, para darle formato
+
+# activa traceback para mejorar la depuración de excepciones
+traceback.install()
+
+# Directorios con rutas corregidas a estilo python
+output_dir = ".//data"
+input_file = ".//data//claves.csv"
 
 
-# Directorios con rutas corregidas
-output_dir = r'C:\Users\laram\OneDrive\Escritorio\repositorio\ai-advance-pe-final-1\data'
-input_file = r'C:\Users\laram\OneDrive\Escritorio\repositorio\ai-advance-pe-final-1\data\claves.csv'
+# Función para generar el texto
+def create_enfermedad_details_text(row):
+    return (
+        f"Enfermedad: {row['nombre']}\n"
+        f"Sintoma Principal: {row['sintoma_1']}\n"
+        f"Sintoma Secundario: {row['sintoma_2']}\n"
+        f"Sintoma Normal: {row['sintoma_3']}\n"
+        f"Sintoma Medio: {row['sintoma_4']}\n"
+        f"Sintoma Bajo: {row['sintoma_5']}\n"
+        f"Sintoma sin importancia: {row['sintoma_6']}\n"
+        f"Solucion: {row['solucion']}\n"
+        f"Medicamento: {row['medicamento']}\n"
+        f"Licencia Medica: {row['licencia_medica']}\n"
+    )
 
 # Crear la carpeta si no existe
 os.makedirs(output_dir, exist_ok=True)
@@ -25,69 +40,58 @@ os.makedirs(output_dir, exist_ok=True)
 # Leer el archivo CSV
 df = pd.read_csv(input_file)
 
-# Función para generar el texto
-def create_enfermedad_details_text(row):
-    return (f"Enfermedad: {row['nombre']}\n"
-            f"Sintoma Principal: {row['sintoma_1']}\n"
-            f"Sintoma Secundario: {row['sintoma_2']}\n"
-            f"Sintoma Normal: {row['sintoma_3']}\n"
-            f"Sintoma Medio: {row['sintoma_4']}\n"
-            f"Sintoma Bajo: {row['sintoma_5']}\n"
-            f"Sintoma sin importancia: {row['sintoma_6']}\n"
-            f"Solucion: {row['solucion']}\n"
-            f"Medicamento: {row['medicamento']}\n"
-            f"Licencia Medica: {row['licencia_medica']}\n")
-
 # Iterar sobre el DataFrame y guardar cada enfermedad como un archivo .txt
 for _, row in df.iterrows():
-    enfermedad_name = row['nombre'].replace("/", "-")  # Asegurar nombres de archivo válidos
+    enfermedad_name = row["nombre"].replace(
+        "/", "-"
+    )  # Asegurar nombres de archivo válidos
     file_name = f"{enfermedad_name}.txt"
     file_path = os.path.join(output_dir, file_name)
-    
+
     # Escribir los detalles en un archivo .txt
-    with open(file_path, 'w', encoding='utf-8') as file:
+    with open(file_path, "w", encoding="utf-8") as file:
         file.write(create_enfermedad_details_text(row))
 
-print(f"Archivos guardados en: {output_dir}")
+print(f"[bold]Archivos guardados en: '{os.path.normpath(os.path.abspath(output_dir))}'.[/bold]")
 
+input_dir = os.path.dirname(input_file)
 
-""
-
-
-input_dir = os.path.normpath('C:\Users\laram\OneDrive\Escritorio\repositorio\ai-advance-pe-final-1\data')
-
-redis_host="redis-19179.c277.us-east-1-3.ec2.redns.redis-cloud.com"
-redis_port="19179"
-redis_db=0
-redis_password="GlTO5JYBYVaT4bGgEKhpAkR2oyxxbyg4"
-redis_username="default"
-redis_index = "V1"
+# Lectura de claves desde el entorno
+redis_host = os.environ.get("REDIS_HOST")
+redis_port = os.environ.get("REDIS_PORT")
+redis_db = os.environ.get("REDIS_DB")
+redis_password = os.environ.get("REDIS_PASSWORD")
+redis_username = os.environ.get("REDIS_USERNAME")
+redis_index = os.environ.get("REDIS_INDEX")
 gpt_key = os.environ.get("OPENAI_API_KEY")
 
-#Crear embeddings usando la API de OpenAI
+# Crear embeddings usando la API de OpenAI
 embeddings = OpenAIEmbeddings(openai_api_key=gpt_key)
 
-#Iterar sobre cada archivo en el directorio y cargarlo a REDIS 
+# Iterar sobre cada archivo en el directorio y cargarlo a REDIS
 for filename in sorted(os.listdir(input_dir)):
     if filename.endswith(".txt"):
-        file_path = os.path.join(input_dir, filename)
+        file_path = os.path.normpath(os.path.join(input_dir, filename))
 
-        #Cargar el documento desde el archivo de texto 
+        # Cargar el documento desde el archivo de texto
         loader = TextLoader(file_path, encoding="utf-8")
         documents = loader.load()
 
-        #Dividir el documento en fragmentos de tamaño adecuado para embeddings 
+        # Dividir el documento en fragmentos de tamaño adecuado para embeddings
         text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
         docs = text_splitter.split_documents(documents)
+        number_chunks = len(docs)
 
-        #Cargar los documentos divididos a REDIS con embeddings
+        # Cargar los documentos divididos a REDIS con embeddings
         vectorstore = Redis.from_documents(
             docs,
             embeddings,
             redis_url=f"redis://{redis_username}:{redis_password}@{redis_host}:{redis_port}/{redis_db}",
-            index_name=redis_index
+            index_name=redis_index,
         )
 
-        print(f"Documentos del archivo '{filename}'cargado exitosamente en REDIS.")
+        print(
+            f"[bold]Documentos del archivo '{filename}' fueron cargados exitosamente en REDIS, en{ {number_chunks}+'chunks' if number_chunks>1 else 'un solo chunk'}.[/bold]"
+        )
 
-print("Todos los archivos han sido procesados y cargados en REDIS.")
+print("[bold green]Todos los archivos han sido procesados y cargados en REDIS.[/bold green]")
